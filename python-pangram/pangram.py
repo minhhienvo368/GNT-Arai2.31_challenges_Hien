1287c4968e05804b5be9cd760b5449ed1c4e04d1
Minh Hien Vo <minh.hien.vo68@gmail.com>
2021-06-07 16:00:56 +0200

    Push pangram.py file

---
+++ python-pangram/pangram.py
@@ -0,0 +1,19 @@
+# Write a program check if a string is a pangram
+import string
+import re
+
+def strip_punctuation(word: str) -> str:  #eleminate puntuation function
+    exclude = set(string.punctuation)
+    word = ''.join(char for char in word if char not in exclude)
+    return word
+
+def is_pangram(text: str) -> bool:
+    text = re.sub('"', '', text)
+    text = text.rstrip()
+    text = strip_punctuation(text.lower())
+    for char in string.ascii_lowercase:
+        if char not in text:
+            return False
+    return True
+
+is_pangram("'Victor jagt zwölf Boxkämpfer quer über den' ' großen Sylter Deich.'")
